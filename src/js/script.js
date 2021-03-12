efc0459cc0a8da0be1f9852df140d3dca68eee34
Andrew <skid.one@inbox.ru>
2021-03-13 01:33:55 +0300

    php

--- src/js/script.js
+++ src/js/script.js
@@ -81,4 +81,18 @@ $(document).ready(function(){
     valideForms('#consultation form');
     valideForms('#order form'); 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type:"POST",
+            url:"mailer/smart.php",
+            data:$(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
   });
